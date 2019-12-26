94d33c656446865ed71d74333faf1c82e96fb9fb
Louis Lu <lewgq@yahoo.com>
2019-12-26 22:14:11 +0800

    These are the modules I build for further usage.

--- demos/calldrawdot.py
+++ demos/calldrawdot.py
@@ -5,30 +5,33 @@
 import sys
 from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.QtCore import Qt
-from demos.demoMouseClicks import *
+from PyQt5.QtGui import QPainter, QPen
+from demos.demoDrawDot import *
 
 class MyForm(QDialog):
     def __init__(self):
         super().__init__()
         self.ui=Ui_Dialog()
         self.ui.setupUi(self)
+        self.pos1=[0,0]
+
+    def paintEvent(self, event):
+        qp=QPainter()
+        qp.begin(self)
+        pen=QPen(Qt.black, 5)
+        qp.setPen(pen)
+        qp.drawPoint(self.pos1[0],self.pos1[1])
+        qp.end()
 
     def mousePressEvent(self, event):
         if event.buttons() and Qt.LeftButton:
-            x = event.x()
-            y = event.y()
-            text = 'x:{}, y:{}'.format(x, y)
-            self.ui.labelPress.setText('Mouse button pressed at ' +text)
-
-    def mouseReleaseEvent(self, event):
-        x = event.x()
-        y = event.y()
-        text = 'x:{}, y:{}'.format(x, y)
-        self.ui.labelRelease.setText('Mouse button released at ' +text)
+            self.pos1[0],self.pos1[1]=event.pos().x(),event.pos().y()
+            self.update()
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
     w = MyForm()
     w.show()
     sys.exit(app.exec_())
+
 
